0734d9f4e3ebfc136db611c34b50cd8e903bb48a
Ezpeasy0 <158174291+Ezpeasy0@users.noreply.github.com>
2026-02-10 17:24:50 +0700

    03 ex00

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -7,7 +7,7 @@ number = int(user_input)
 if number > 25:
     print("Error")
 else:
-    # วนลูปจากเลขที่รับมาไปจนถึง 25
+
     while number <= 25:
         print(f"Inside the loop, my variable is {number}")
         number += 1
